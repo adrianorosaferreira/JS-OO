add35d2bca60d2bafeededa00e8110e5c2b9f1e0
Adriano da Rosa Ferreira <adrianorosaferreira@gmail.com>
2019-03-25 11:36:29 -0300

    Changed DateHelper functions for static

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,10 +1,14 @@
-class DataHelper {
+class DateHelper {
 
-    dataParaTexto(data) {
+    constructor() {
+        throw new Error('Esta classe não pode ser instanciada');
+    }
+
+    static dataParaTexto(data) {
         return data.getDate() + '/' + (data.getMonth() + 1) + '/' + data.getFullYear();
     }
 
-    textoParaData(texto) {
+    static textoParaData(texto) {
         return new Date(...texto
             .split('-')
             .map((data, index) => data - index % 2));
