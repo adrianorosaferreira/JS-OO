ed0b6ca9e653ade448807c5cf50fe84a597c85cd
Adriano da Rosa Ferreira <adrianorosaferreira@gmail.com>
2019-03-28 09:37:40 -0300

    Applied Design Pattern Proxy on NegociacoesController

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,8 +6,26 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes(model =>
-            this._gridNegociacoes.update(this._listaNegociacoes));
+
+        let self = this;
+        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+
+            get(target, prop, receiver) {
+
+                if (['adiciona', 'apagar'].includes(prop) && typeof(target[prop]) == typeof(Function)) {
+
+                    return function() {
+                        console.log(`interceptado ${prop}`);
+                        Reflect.apply(target[prop], target, arguments);
+                        self._gridNegociacoes.update(target);
+                    }
+
+                }
+
+                return Reflect.get(target, prop, receiver);
+            }
+        });
+
         this._gridNegociacoes = new NegociacaoView($('#grid-negociacoes'));
         this._gridNegociacoes.update(this._listaNegociacoes);
 
